c38efb231b7c1896b7ca9b03244f0950f9555851
AnnyeongAloha <anneyoreaper@gmail.com>
2023-02-08 02:42:45 +0000

    Challenge Completed

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution 
+# Author: 
+# Date of latest revision:
+# Purpose: Print a string to the terminal 
+
+
+
+# Main
+var='I am working perfectly fine'
+echo $var
+
+# End

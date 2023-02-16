ca18df74300b390cb456719919f2f7fdaa8ff0aa
AnnyeongAloha <116544291+AnnyeongAloha@users.noreply.github.com>
2023-02-15 23:09:42 -0800

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 08 Ops Challenge Solution 
+# Author: Justin 'Sage' Tabios
+# Date of latest revision:
+# Purpose: Automate the copy operation for Jorge
+
+
+# Main
 ROBOCOPY "C:\Users\j.thompson\Desktop\My Project Files" "E:\backups" /mir /E 
+
+
+# End
+

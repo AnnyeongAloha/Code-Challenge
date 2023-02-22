f50d19f50d1df66a1e5851c07672968ca38f9ef3
AnnyeongAloha <116544291+AnnyeongAloha@users.noreply.github.com>
2023-02-21 21:23:15 -0800

    Update Ops201d6-day12.ps1

--- Ops201d6-day12.ps1
+++ Ops201d6-day12.ps1
@@ -1,3 +1,8 @@
+# Script: Ops 201 Class 12 Ops Code Challenge Solution
+# Author: Justin 'Sage' Tabios
+# Date of latest revision: 21 Feb 2023
+# Purpose: Create a local file called network_report.txt and return only the IP version 4 address.
+
 $ip_output = ipconfig >> "network_report.txt"
 $reset_dhcp = ipconfig /release; ipconfig /renew
 
